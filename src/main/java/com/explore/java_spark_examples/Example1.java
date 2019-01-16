f7bc824d7431613c410157a1c8a090d901bdbcfa
Lucky Pratama <luckypratama71@yahoo.com>
2019-01-17 02:29:55 +0700

    Initial commit

--- src/main/java/com/explore/java_spark_examples/Example1.java
+++ src/main/java/com/explore/java_spark_examples/Example1.java
@@ -14,38 +14,33 @@ import org.apache.spark.sql.Row;
  */
 
 public class Example1 {
-	
+
 	private static final String dataPath = "src/main/resources/dataset/animal.txt";
-	
-	public static void main (String[] args) {
-		//Set hadoop environment
-		//If you're using windows, you could download winutils from https://github.com/steveloughran/winutils
+
+	public static void main(String[] args) {
+		// Set hadoop environment
+		// If you're using windows, you could download winutils from
+		// https://github.com/steveloughran/winutils
 		System.setProperty("hadoop.home.dir", "D://Hadoop/hadoop-3.0.0");
-		
-		//Configure Spark Session
-		SparkSession spark = SparkSession
-							 .builder()
-							 .master("local[*]")
-							 .appName("Example1")
-							 .getOrCreate();
-		
-		//Configure Dataset
-		Dataset<Row> dataFrame = spark.read()
-							.option("header", "true")
-							.csv(dataPath);
-		
-		//Group data by group and age
+
+		// Configure Spark Session
+		SparkSession spark = SparkSession.builder().master("local[*]").appName("Example1").getOrCreate();
+
+		// Configure Dataset
+		Dataset<Row> dataFrame = spark.read().option("header", "true").csv(dataPath);
+
+		// Group data by group and age
 		Dataset<Row> groupAnimalwithSameAge = dataFrame.groupBy("Group", "Age").count().select("Group", "count");
 		groupAnimalwithSameAge.show();
-		
+
 		try {
-			//Keep the spark web ui alive for 1 minute
-			Thread.sleep(60000);		
-		}catch (InterruptedException e) {
+			// Keep the spark web ui alive for 1 minute
+			Thread.sleep(60000);
+		} catch (InterruptedException e) {
 			e.printStackTrace();
 		} finally {
-		  //Stop spark
-		  spark.close();
+			// Stop spark
+			spark.close();
 		}
 	}
 }
